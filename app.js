0805e31c57b26f5a14048dae937b17f4e6a99604
wfem12 <wfem12@hotmail.com>
2023-01-12 11:35:39 -0600

    api agregado
    Se agrego y se le coloco un hola mundo por mientras

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// ARCHIVOS A LLAMAR
+var apiRouter = require('./routes/api/api');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -14,7 +16,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// ARCHIVOS A EJECUTAR
 app.use('/', indexRouter);
+app.use('/api', apiRouter);
 app.use('/users', usersRouter);
 
 module.exports = app;
